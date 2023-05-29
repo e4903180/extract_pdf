9eb222684284d9ca844e8032229650ed8ca1add7
e4903180 <e4903180@gmail.com>
2023-05-29 13:51:21 +0800

    add Fubon

--- ExtractPdf.py
+++ ExtractPdf.py
@@ -9,6 +9,9 @@ class ExtractPdf(object):
             rating_1 : (str) rating extracted by method 1
             rating_2 : (str) rating extracted by method 2
             possible_rating : (list) all possible ratings in the adviser
+
+        Todo:
+            fix get tp, author process
         '''
         self.file_path = file_path
         self.rating_1 = 'NULL'
@@ -51,6 +54,22 @@ class ExtractPdf(object):
             return tp_2
         else:
             return 'NULL'
+        # else:
+        #     if '.' in tp_1:
+        #         try:
+        #             int = int(tp_1.split('.')[0])
+        #             int = int(tp_1.split('.')[1])
+        #             return tp_1
+        #         except:
+        #             pass
+        #     if '.' in tp_2:
+        #         try:
+        #             int = int(tp_2.split('.')[0])
+        #             int = int(tp_2.split('.')[1])
+        #             return tp_2
+        #         except:
+        #             pass
+        #     return 'NULL'
 
     def check_author(self, author_1, author_2):
         '''Check that the extracted target_price are correct
@@ -66,6 +85,13 @@ class ExtractPdf(object):
             return author_2
         else:
             return 'NULL'
+        # else:
+        #     if len(author_1)>0 and len(author_1)<4:
+        #         return author_1
+        #     elif len(author_2)>0 and len(author_2)<4:
+        #         return author_2
+        #     else:
+        #         return 'NULL'
 
     def check_summary(self, summary_1, summary_2):
         '''Check that the extracted target_price are correct
